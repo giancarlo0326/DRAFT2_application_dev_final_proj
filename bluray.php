b2fa37ef4f27e7d3b182441cb41ea94a1654a4a9
Gian Carlo S. Victorino <155649616+giancarlo0326@users.noreply.github.com>
2024-06-14 16:02:51 +0800

    Add files via upload
    added a hero section in the main page

---
+++ bluray.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blu-Rays - PUIHAHA Videos</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        h1 {
+            color: #fff;
+            font-size: 100px;
+            text-align: center;
+            margin-top: 20vh; /* Adjust the top margin as needed */
+        }
+
+        .typed-text {
+            color: #c66515;
+        }
+
+        @media (max-width: 768px) {
+            h1 {
+                font-size: 4rem;
+            }
+        }
+
+        body {
+            position: relative; /* Ensure body is relative for absolute positioning inside */
+        }
+
+        .hero-content {
+            position: relative; /* Use relative positioning instead of absolute */
+            text-align: center; /* Center align content */
+            padding: 10vh 10vh; /* Adjust padding for top and bottom spacing */
+        }
+
+    </style>
+</head>
+<body>
+    <nav>
+        <a class="home-link" href="index.php">
+            <img src="https://i.postimg.cc/CxLnK8q1/PUIHAHA-VIDEOS.png" alt="Home">
+        </a>
+        <input type="checkbox" id="sidebar-active">
+        <label for="sidebar-active" class="open-sidebar-button">
+            <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+        </label>
+        <label id="overlay" for="sidebar-active"></label>
+        <div class="links-container">
+            <label for="sidebar-active" class="close-sidebar-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+            </label>
+            <a href="dvd.php">DVDs</a> 
+            <a href="bluray.php">Blu-Rays</a>
+            <a href="digitals.php">Digitals</a>
+            <a href="viewallcatalogs.php">View All Catalogs</a>
+            <a href="viewrentals.php">View Rentals</a>
+            <a href="aboutdevs.php">About the Devs</a>
+            <a href="login.php">Login</a>
+        </div>
+    </nav>
+
+    <div class="hero-content">
+        <h1>Rent <span class="auto-type typed-text"></span></h1>
+        <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+        <script>
+            var typed = new Typed(".auto-type",{
+                strings: ["DVDs", "Blu-Rays", "Digitals", "almost anything.", "with PUIHAHA Videos."],
+                typeSpeed: 100,
+                backSpeed: 10,
+                loop: true
+            });
+        </script>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
